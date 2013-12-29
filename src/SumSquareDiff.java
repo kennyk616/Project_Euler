6007365792f8757c055d3d424d85b69b79b5fd97
Ken Kao <kennyk616@gmail.com>
2013-12-29 14:26:14 +0800

    complete sum square difference

---
+++ src/SumSquareDiff.java
@@ -0,0 +1,18 @@
+/**
+ * Sum Square Difference
+ * http://projecteuler.net/problem=6
+ * @author ken
+ *
+ */
+public class SumSquareDiff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 100;
+		int val = n*(3*n+2)*((int)(Math.pow(n, 2))-1)/12;
+		System.out.println(val);
+
+	}
+
+}
