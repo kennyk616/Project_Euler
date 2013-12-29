d3c338ee30e25ebcfb85f00ab2e64015ac90885f
Ken Kao <kennyk616@gmail.com>
2013-12-29 13:27:32 +0800

    complete largest prime factor

---
+++ src/LargestPrimeFactor.java
@@ -0,0 +1,46 @@
+/**
+ * Find the largest prime factor of a value
+ * @author ken
+ *
+ */
+public class LargestPrimeFactor {
+
+	public static long largestPrimeFactor(long n) {
+		long val = n;
+		long divisor = 2L;
+		long largestDivisor = 2L;
+		while (val >1) {
+			if(isPrime(divisor)){
+				if (val%divisor == 0L) {
+					largestDivisor = Math.max(largestDivisor, divisor);
+					val = val/divisor;
+				}else{
+					divisor++;
+				}
+			}else{
+				divisor++;
+			}
+		}
+
+		return largestDivisor;
+	}
+	
+	public static boolean isPrime(long n) {
+		long sqrt = (long)(Math.sqrt(n));
+		for (long i = 2; i<=sqrt; i++) {
+			if (n%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long n = 600851475143L;
+		System.out.println(largestPrimeFactor(n));
+		System.exit(0);
+
+	}
+
+}
