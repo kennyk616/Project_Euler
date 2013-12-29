430c10795855da74491bbec2df8b55f3afde3e26
Ken Kao <kennyk616@gmail.com>
2013-12-29 20:20:17 +0800

    complete 25

--- src/Problem025.java
+++ src/Problem025.java
@@ -1,44 +1,32 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.math.BigInteger;
 
 /**
- * lexicographic permutations
- * http://projecteuler.net/problem=24
+ * 1000-digit Fib number
+ * http://projecteuler.net/problem=25
  * @author ken
  *
  */
 public class Problem025 {
-	
-	public static int factorial(int n){
-		if (n==0) return 1;
-		int prod = 1;
-		for (int i = 2; i<=n; i++){
-			prod = prod*i;
-		}
-		return prod;
-	}
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 1000000;
-		StringBuilder builder = new StringBuilder();
-		List<Integer> digits = new LinkedList<Integer>(Arrays.asList(new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
-		
-		int val = n-1;
-		int fixDigit = 9;
-		while(fixDigit>=0) {
-			int permFixDigit = factorial(fixDigit);
-			int idx = val/permFixDigit;
-			builder.append(digits.get(idx));
-			digits.remove(idx);
-			val = val %permFixDigit;
-			fixDigit--;
+		int n = 1000;
+		BigInteger prev = BigInteger.valueOf(1);
+		BigInteger cur = BigInteger.valueOf(1);
+		int count = 2;
+		while(true) {
+			int length = cur.toString().length();
+			if (length ==n){
+				break;
+			}
+			BigInteger next = cur.add(prev);
+			prev = cur;
+			cur = next;
+			count ++;
 		}
-		
-		System.out.println(new String(builder));
+		System.out.println(count);
 		System.exit(0);
+		
 	}
 
 }
