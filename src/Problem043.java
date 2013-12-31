98d650a1e843b2fda1ab1a990cbc22fd21a46a7f
Ken Kao <kennyk616@gmail.com>
2013-12-31 19:06:16 +0800

    complete 43

---
+++ src/Problem043.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * substring divisibiliby
+ * http://projecteuler.net/problem=43
+ * @author ken
+ *
+ */
+public class Problem043 {
+	/*
+	 * can be done by hand calculation.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long[] values = new long[]{1430952867, 4130952867L, 1460357289, 4160357289L, 1406357289, 4106357289L};
+		long sum = 0;
+		for (long val : values){
+			sum += val;
+		}
+		
+		System.out.println(sum);
+		System.exit(0);
+		
+	}
+
+}
